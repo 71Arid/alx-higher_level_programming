af25a5c6a515badb7ec84b02e45493c9a7b4ff2e
71Arid <brian87noel@gmail.com>
2023-03-21 16:21:57 +0300

    new

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv)
+    if argc == 1:
+        print(f"0 arguments.")
+    else:
+        print(f"{argc - 1} arguments:")
+
+    for i in range(1, argc):
+        print(f"{i}: {sys.argv[i]}")
