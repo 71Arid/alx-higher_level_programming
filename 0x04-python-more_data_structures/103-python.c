d87758a186bcc31d5749347438223a9034fba82c
71Arid <brian87noel@gmail.com>
2023-03-24 21:55:15 +0300

    cpy

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,67 +1,46 @@
 #include <Python.h>
 
 void print_python_list(PyObject *p) {
-    Py_ssize_t len, i;
+    Py_ssize_t len = PyList_Size(p);
     PyObject *item;
+    PyTypeObject *type;
     const char *type_name;
+    Py_ssize_t i;
 
-    if (!PyList_Check(p)) {
-        printf("Invalid List Object\n");
-        return;
-    }
-
-    len = PyList_Size(p);
     printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %zd\n", len);
-    printf("[*] Allocated = %zd\n", ((PyListObject *) p)->allocated);
+    printf("[*] Size of the Python List = %ld\n", len);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < len; i++) {
         item = PyList_GetItem(p, i);
-        type_name = Py_TYPE(item)->tp_name;
-        printf("Element %zd: %s\n", i, type_name);
-        if (PyBytes_Check(item)) {
-            printf("  [ERROR] Invalid Bytes Object\n");
+        type = Py_TYPE(item);
+        type_name = type->tp_name;
+        printf("Element %ld: %s\n", i, type_name);
+        if (strcmp(type_name, "bytes") == 0) {
+            print_python_bytes(item);
         }
     }
 }
 
 void print_python_bytes(PyObject *p) {
-    Py_ssize_t size, i;
-    char *buffer;
-    PyObject *bytes;
-    const char *hex_digits = "0123456789abcdef";
-
-    if (!PyBytes_Check(p)) {
-        printf("Invalid Bytes Object\n");
-        return;
-    }
+    PyBytesObject *bytes = (PyBytesObject *)p;
+    Py_ssize_t len = PyBytes_Size(p);
+    char *str = PyBytes_AsString(p);
+    Py_ssize_t i;
 
     printf("[.] bytes object info\n");
-    printf("  size: %zd\n", PyBytes_Size(p));
-
-    bytes = PyBytes_FromStringAndSize(NULL, PyBytes_Size(p));
-    if (!bytes) {
-        printf("  [ERROR] Memory allocation failed\n");
-        return;
-    }
-
-    size = PyBytes_AsStringAndSize(bytes, &buffer);
-    if (size < 0) {
-        printf("  [ERROR] Conversion to bytes failed\n");
-        Py_DECREF(bytes);
-        return;
-    }
-
-    printf("  trying string: %s\n", buffer);
-
-    printf("  first %zd bytes: ", size < 10 ? size : 10);
-    for (i = 0; i < (size < 10 ? size : 10); i++) {
-        printf("%c%c", hex_digits[(unsigned char) buffer[i] >> 4], hex_digits[(unsigned char) buffer[i] & 15]);
-        if (i < 9) {
-            printf(" ");
+    if (PyBytes_Check(p)) {
+        printf("  size: %ld\n", len);
+        printf("  trying string: %s\n", str);
+        printf("  first %ld bytes: ", len <= 10 ? len : 10);
+        for (i = 0; i < len && i < 10; i++) {
+            printf("%02hhx", str[i]);
+            if (i < len - 1 && i < 9) {
+                printf(" ");
+            }
         }
+        printf("\n");
+    } else {
+        printf("  [ERROR] Invalid Bytes Object\n");
     }
-    printf("\n");
-
-    Py_DECREF(bytes);
 }
