836790d8b2fccbbe855eda1d1ce253bee5068a30
71Arid <brian87noel@gmail.com>
2023-04-07 21:28:29 +0300

    all

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2-matrix_divided.py
+"""Defines a matrix division function."""
+
+
+def matrix_divided(matrix, div):
+    """ Returns new matrix representing the result of division """
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
