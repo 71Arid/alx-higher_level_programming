258914043eebe0c182ab73102b7ae859abdfd250
71Arid <brian87noel@gmail.com>
2023-06-08 16:02:26 +0300

    new

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""lookup module"""
 
 def lookup(obj):
+"""finds attributes in dictionaries"""
+
     list = []
     obj_atrributes = obj.__dict__.keys()
     for obj_attribute in obj_atrributes:
