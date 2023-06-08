950431dbdb7040a715ee595d9268034ca4a84a90
71Arid <brian87noel@gmail.com>
2023-06-08 21:22:27 +0300

    new

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,8 +5,4 @@
 def lookup(obj):
     """finds attributes in dictionaries"""
 
-    list = []
-    obj_atrributes = obj.__dict__.keys()
-    for obj_attribute in obj_atrributes:
-        list.append(obj_attribute)
-    return list
+    return (dir(obj))
