7c56dc33fd5e206c441463f3a66d0de826eb9a36
71Arid <brian87noel@gmail.com>
2023-06-20 10:38:07 +0300

    js warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,4 @@
 const args = process.argv;
 
 const num = parseInt(args[2]);
-if (isNaN(num)) { console.log('Not a number'); } else { console.log('Number: ', num); }
+if (isNaN(num)) { console.log('Not a number'); } else { console.log('My number: ', num); }
