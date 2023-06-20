97b28e3020f00fb46035908b95756a6b51f75f61
71Arid <brian87noel@gmail.com>
2023-06-20 11:47:40 +0300

    js warm up

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr() = {
+  let num = myObject.value;
+  return ++num;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
