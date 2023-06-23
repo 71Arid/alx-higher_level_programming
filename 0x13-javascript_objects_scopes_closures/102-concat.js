c75e80adeaf7c2195d00a2fe2d6a23158adf27db
71Arid <brian87noel@gmail.com>
2023-06-23 15:50:17 +0300

    jj

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 
 const a = fs.readFileSync(args[2], 'utf8');
 const b = fs.readFileSync(args[3], 'utf8');
-const c = a + b;
+const c = a + '\n' + b;
 
 fs.writeFileSync(args[4], c);
-const out = fs.readFileSync(args[4], 'utf8');
-console.log(out);
