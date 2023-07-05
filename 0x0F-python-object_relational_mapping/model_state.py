dda944eacc4eb614f3db32e8a0c3829ee1ad6fa6
71Arid <brian87noel@gmail.com>
2023-07-05 10:21:01 +0300

    ORMs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 Base = declarative_base()
+
+
 class State(Base):
     """ definition of mysql states table """
     __tablename__ = 'states'
