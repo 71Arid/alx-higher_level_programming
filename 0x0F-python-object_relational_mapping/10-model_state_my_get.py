ec6b00cbe4cca555812968e6b235db4761b864a0
71Arid <brian87noel@gmail.com>
2023-07-05 11:19:38 +0300

    ORMs

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,7 +8,8 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3])
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+      sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(url)
     Session = sessionmaker(bind=engine)
     session = Session()
