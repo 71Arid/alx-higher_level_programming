5a8cb5d6762adaf7865680c5f6455902c24de1cc
71Arid <no232el@gmail.com>
2023-07-26 19:58:06 -0700

    ll

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script sends POST request display body of response
+# script to send POST request and displays body
 curl -sX POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
