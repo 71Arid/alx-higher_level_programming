2cf8bdf810e7647eb73b1a1cf0c899d02171a132
71Arid <no232el@gmail.com>
2023-07-26 20:32:42 -0700

    u

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# find peak function
+def find_peak(list_of_integers):
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
